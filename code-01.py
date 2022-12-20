b50b184447f4af0931439c7834e2fc012b468478
Igor Volkov <telematrix.synthecity@gmail.com>
2022-12-20 13:47:14 +0300

    some changes were done again

--- code-01.py
+++ code-01.py
@@ -1,2 +1,3 @@
 print('This is project created for GIT learning')
 print('')
+print('This is some changes in code-01')
--- code-02.py
+++ code-02.py
@@ -1,1 +1,3 @@
 print('This is changes in code-02 file')
+print()
+print('This is some new changes in code-02')
